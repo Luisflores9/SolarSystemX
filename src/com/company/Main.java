67f449d2c905dfbd09febbf8d0a7cdc904dc7a6b
Luisflores9 <Luis.fsanchez09@gmail.com>
2022-08-30 17:11:09 -0400

    Solar System ready to be deploay

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+      solarSystem inception = new solarSystem();
+      inception.Sun=1;
+      inception.Moon=2;
+      inception.Planets=2;
+
+        System.out.println("the basics structure is ready");
+      
     }
 }
